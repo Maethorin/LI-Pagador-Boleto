970925dde83f2c8b8aabfe0cfb96abc7d10eaffe
Márcio Duarte <maethorin@gmail.com>
2014-10-07 15:52:59 -0300

    Boleto exibindo linha digitável no checkout

--- src/pagador_boleto/extensao/requisicao.py
+++ src/pagador_boleto/extensao/requisicao.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+import datetime
 from pagador.configuracao.models import Banco
 
 from pagador.envio.requisicao import Enviar
@@ -32,28 +33,35 @@ class EnviarPedido(Enviar):
     def obter_situacao_do_pedido(self, status_requisicao):
         return SituacaoPedido.SITUACAO_AGUARDANDO_PAGTO
 
+    @property
+    def endereco_completo(self):
+        complemento = self.pedido.endereco_pagamento.complemento and (" - %s" % self.pedido.endereco_pagamento.complemento) or ""
+        return "{}, {}{} - {}, {} / {} - CEP: {}".format(
+            self.pedido.endereco_pagamento.endereco, self.pedido.endereco_pagamento.numero, complemento, self.pedido.endereco_pagamento.bairro,
+            self.pedido.endereco_pagamento.cidade, self.pedido.endereco_pagamento.estado, self.pedido.endereco_pagamento.cep)
+
     def processar_resposta(self, resposta):
-        try:
-            pagamento_venda = self.pedido.pedido_venda_pagamento_da_forma_de_pagamento(pagamento_id=7)[0]
-        except IndexError:
-            return {"content": "Não foi encontrada forma de pagamento usando depósito bancário para o pedido {} na conta {}".format(self.pedido.numero, self.pedido.conta_id), "status": 404, "reenviar": False}
-        pagamento_banco = pagamento_venda.banco
-        email_comprovante = self.configuracao_pagamento.email_comprovante
-        if not email_comprovante:
-            email_comprovante = self.pedido.conta.email_contato
+        if self.pedido.pagamento.codigo != 'boleto':
+            return {"content": "Não foi encontrada forma de pagamento usando boleto bancário para o pedido {} na conta {}".format(self.pedido.numero, self.pedido.conta_id), "status": 404, "reenviar": False}
+        sacado = [self.pedido.endereco_entrega.nome, self.endereco_completo]
+        boleto = {}
+        resultado = self.emitir_boleto(
+            self.pedido.data_criacao.date(),
+            self.pedido.data_criacao.date(),
+            self.pedido.data_criacao.date() + datetime.timedelta(days=5),
+            self.pedido.valor_total,
+            sacado, self.pedido.numero,
+            tipo=self.dados["tipo_boleto"]
+        )
+        if self.dados["tipo_boleto"] == TipoBoleto.linha_digitavel:
+            boleto = {"linha_digitavel": resultado}
+        if self.dados["tipo_boleto"] == TipoBoleto.html:
+            boleto = {"html": resultado}
+        if self.dados["tipo_boleto"] == TipoBoleto.pdf:
+            boleto = {"pdf": resultado}
         return {
             "content": {
-                "nome": pagamento_banco.banco.nome,
-                "codigo": pagamento_banco.banco.codigo,
-                "imagem": pagamento_banco.banco.imagem,
-                "agencia": pagamento_banco.agencia,
-                "numero_conta": pagamento_banco.numero_conta,
-                "poupanca": pagamento_banco.poupanca,
-                "favorecido": pagamento_banco.favorecido,
-                "cpf": pagamento_banco.cpf,
-                "cnpj": pagamento_banco.cnpj,
-                "email_comprovante": email_comprovante,
-                "informacao_complementar": self.configuracao_pagamento.informacao_complementar,
+                "boleto": boleto
             },
             "status": 200,
             "reenviar": False
--- src/pagador_boleto/extensao/templates/javascript.js
+++ src/pagador_boleto/extensao/templates/javascript.js
@@ -3,16 +3,16 @@ var url = '';
 var $counter = null;
 var segundos = 5;
 $(function() {
-    var $depositoMensagem = $(".deposito-mensagem");
+    var $boletoMensagem = $(".boleto-mensagem");
 
     function enviaPagamento() {
-        $depositoMensagem.find(".msg-danger").hide();
-        $depositoMensagem.find(".msg-success").hide();
-        $depositoMensagem.find(".msg-warning").show();
-        $depositoMensagem.removeClass("alert-message-success");
-        $depositoMensagem.removeClass("alert-message-danger");
-        $depositoMensagem.addClass("alert-message-warning");
-        var url_pagar = '{% url_loja "checkout_pagador" pedido.numero pagamento.id %}';
+        $boletoMensagem.find(".msg-danger").hide();
+        $boletoMensagem.find(".msg-success").hide();
+        $boletoMensagem.find(".msg-warning").show();
+        $boletoMensagem.removeClass("alert-message-success");
+        $boletoMensagem.removeClass("alert-message-danger");
+        $boletoMensagem.addClass("alert-message-warning");
+        var url_pagar = '{% url_loja "checkout_pagador" pedido.numero pagamento.id %}?tipo_boleto=linha_digitavel';
         $.getJSON(url_pagar)
             .fail(function (data) {
                 exibeMensagemErro(data.status, data.content);
@@ -21,7 +21,7 @@ $(function() {
                 console.log(data);
                 if (data.sucesso) {
                     $("#aguarde").hide();
-                    exibeMensagemSucesso(data.content)
+                    exibeMensagemSucesso(data.content.boleto)
                 }
                 else {
                     exibeMensagemErro(data.status, data.content);
@@ -30,44 +30,20 @@ $(function() {
     }
 
     function exibeMensagemErro(status, mensagem) {
-        $depositoMensagem.find(".msg-warning").hide();
-        $depositoMensagem.toggleClass("alert-message-warning alert-message-danger");
+        $boletoMensagem.find(".msg-warning").hide();
+        $boletoMensagem.toggleClass("alert-message-warning alert-message-danger");
         var $errorMessage = $("#errorMessage");
         $errorMessage.text(status + ": " + mensagem);
-        $depositoMensagem.find(".msg-danger").show();
+        $boletoMensagem.find(".msg-danger").show();
     }
 
-    function exibeMensagemSucesso(banco) {
-        $depositoMensagem.find(".msg-warning").hide();
-        $depositoMensagem.toggleClass("alert-message-warning alert-message-success");
-        var $success = $depositoMensagem.find(".msg-success");
-        var $dadosBanco = $success.find("#successMessage");
-        var url_imagem = '{{ ADMIN_STATIC_URL }}img/formas-de-pagamento/' + banco.imagem;
-        $dadosBanco.find("#bancoImagem").attr("src", url_imagem);
-        $dadosBanco.find("#bancoNome").text(banco.nome);
-        $dadosBanco.find("#bancoCodigo").text(banco.codigo);
-        $dadosBanco.find("#bancoAgencia").text(banco.agencia);
-        $dadosBanco.find("#bancoConta").text(banco.numero_conta);
-        $dadosBanco.find("#bancoPoupanca").hide();
-        if (banco.poupanca) {
-            $dadosBanco.find("#bancoPoupanca").show();
-        }
-        if (banco.cpf) {
-            $dadosBanco.find("#bancoNomeDocumento").text("CPF:");
-            $dadosBanco.find("#bancoCpfCnpj").text(banco.cpf);
-        }
-        else if (banco.cnpj) {
-            $dadosBanco.find("#bancoNomeDocumento").text("CNPJ:");
-            $dadosBanco.find("#bancoCpfCnpj").text(banco.cnpj);
-        }
-        else {
-            $dadosBanco.find("#documentoNome").hide();
-            $dadosBanco.find("#bancoCpfCnpj").hide();
-        }
-        $dadosBanco.find("#bancoFavorecido").text(banco.favorecido);
-        $dadosBanco.find("#bancoEmailComprovante").text(banco.email_comprovante);
-        $dadosBanco.find("#bancoInformacoesComplementares").text(banco.informacao_complementar);
-        $dadosBanco.show();
+    function exibeMensagemSucesso(boleto) {
+        $boletoMensagem.find(".msg-warning").hide();
+        $boletoMensagem.toggleClass("alert-message-warning alert-message-success");
+        var $success = $boletoMensagem.find(".msg-success");
+        var $dadosBoleto = $success.find("#successMessage");
+        $dadosBoleto.find("#linhaDigitavel").text(boleto.linha_digitavel);
+        $dadosBoleto.show();
         $success.show();
     }
 
