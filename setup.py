63c1d5422e1880d4e111bb2c028a350387ce1428
Márcio Duarte <maethorin@gmail.com>
2014-10-17 12:28:11 -0300

    Versionando

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='li-pagador-boleto',
-    version='0.0.9',
+    version='0.0.10',
     url='https://github.com/lojaintegrada/LI-Pagador-Boleto',
     license='MIT',
     description=u'Meio de pagamento usando boleto bancário',
