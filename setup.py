79239eefd78ea15889edea0d719bb1e5e228ae70
Udlei Nattis <udlei@nati.biz>
2014-10-31 17:59:10 -0200

    commit pagador

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='li-pagador-boleto',
-    version='0.0.16',
+    version='0.0.17',
     url='https://github.com/lojaintegrada/LI-Pagador-Boleto',
     license='MIT',
     description=u'Meio de pagamento usando boleto bancário',
