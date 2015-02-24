8619ae773cf288aed55f34b4c7116e707dc949ce
Jeferson Farias Calazans <calazans10@gmail.com>
2015-02-24 10:55:09 -0300

    Erro de ortografia no botão de imprimir boleto #26919890133179

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='li-pagador-boleto',
-    version='1.0.12',
+    version='1.0.13',
     url='https://github.com/lojaintegrada/LI-Pagador-Boleto',
     license='MIT',
     description=u'Meio de pagamento usando boleto bancário',
