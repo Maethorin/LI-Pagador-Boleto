ce4fc90109734bf29e2aad547b969ad576f0fba3
maethorin <maethorin@gmail.com>
2015-03-05 17:58:30 -0300

    Passando dados para EntregaPagamento

--- src/pagador_boleto/reloaded/servicos.py
+++ src/pagador_boleto/reloaded/servicos.py
@@ -21,7 +21,7 @@ class BoletoInvalido(Exception):
 
 class EntregaPagamento(servicos.EntregaPagamento):
     def __init__(self, loja_id, plano_indice=1, dados=None):
-        super(EntregaPagamento, self).__init__(loja_id, plano_indice, dados=None)
+        super(EntregaPagamento, self).__init__(loja_id, plano_indice, dados=dados)
         self.tem_malote = True
 
     def processa_dados_pagamento(self):
