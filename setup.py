3bf626c908cad9a68808a2b223f71bf2ca072d22
maethorin <maethorin@gmail.com>
2015-03-09 16:14:06 -0300

    Versionando

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ def read(fname):
 
 setup(
     name='li-pagador-boleto',
-    version='2.0.0',
+    version='2.0.1',
     url='https://github.com/lojaintegrada/LI-Pagador-Boleto',
     license='MIT',
     description=u'Meio de pagamento usando boleto bancário',
