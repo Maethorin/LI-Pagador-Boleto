82809bf4d8021ebc77bd97202f1cbadd7a4e4857
maethorin <maethorin@gmail.com>
2015-03-25 18:11:48 -0300

    Corrigindo erro de query a cada loop na listagem parte 1

--- src/pagador_boleto/entidades.py
+++ src/pagador_boleto/entidades.py
@@ -101,7 +101,7 @@ class ConfiguracaoMeioPagamento(entidades.ConfiguracaoMeioPagamento):
         self.campos = ['ativo', 'valor_minimo_aceitado', 'desconto', 'desconto_valor', 'aplicar_no_total', 'json']
         self.codigo_gateway = CODIGO_GATEWAY
         self.eh_gateway = True
-        super(ConfiguracaoMeioPagamento, self).__init__(loja_id, codigo_pagamento, eh_listagem)
+        super(ConfiguracaoMeioPagamento, self).__init__(loja_id, codigo_pagamento, eh_listagem=eh_listagem)
         self.formulario = cadastro.FormularioBoleto()
         if not self.json:
             self.json = cadastro.BOLETO_BASE
